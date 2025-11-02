024d3f91bf41066a6450527a6f70fedcd5064ae5
Queirozzzzzzzzzz <vinicius.queiroz.coding@gmail.com>
2025-11-02 12:24:31 -0300

    hell yeah

--- index.js
+++ index.js
@@ -110,4 +110,5 @@ async function sendMessage(to, text) {
   );
 }
 
-app.listen(3000, () => console.log("🚀 Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("🚀 Server running on port 3000"));
